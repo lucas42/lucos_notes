15a2420f3ca92ffb21ab6a211ef290e867d8c1cb
Luke Blaney <git@lukeblaney.co.uk>
2022-05-22 18:29:54 +0100

    now that v1 -> v2 migration is complete, remove any logic needed for that

--- src/server/statefs.js
+++ src/server/statefs.js
@@ -10,7 +10,6 @@ const state = new State(writeToFS);
 async function init() {
 	const rawData = await readFromFS();
 	state.setRawData(rawData);
-	writeToFS(); // Temporary write so that the migrated state gets written back to disk
 }
 
 init();
@@ -19,8 +18,7 @@ async function readFromFS() {
 	try {
 		return JSON.parse(await readFile(STATE_FILE));
 	} catch (err) {
-		console.log(`Can't find or parse data_v2.json; Trying to convert v1 data`, err);
-		return v2Migration(process.env.STATE_DIR);
+		console.log(`Can't find or parse data_v2.json;`, err);
 	}
 }
 
@@ -29,6 +27,7 @@ async function writeToFS() {
 		await writeFile(STATE_FILE, JSON.stringify(await state.getRawData()));
 	} catch (error) {
 		console.error("Error saving state:", error);
+		throw error;
 	}
 }
 
@@ -45,91 +44,6 @@ export async function getInfoCheck() {
 			ok: false,
 			debug: error.message,
 		}
-	}
-}
-
-/**
- * Transforms the state file format used in v1 to the v2 format
- **/
-async function v2Migration (STATE_DIR) {
-	try {
-		const V1_STATE_FILE = `${STATE_DIR}/data.json`;
-		const v1Data = JSON.parse(await readFile(V1_STATE_FILE));
-		const data = {
-			lists: {},
-			items: {},
-		}
-		for (let path in v1Data) {
-			const val = v1Data[path];
-			const parts = path.split("/");
-			const prefix = parts.shift();
-			if (prefix) {
-				console.warn(`path doesn't begin with leading slash ${path}`);
-				continue;
-			}
-			const category = parts.shift();
-			if (category === ".admin") {
-				continue;
-			}
-			if (category !== "todo") {
-				console.warn(`unrecognised entry "${path}" (val="${val}")`);
-				continue;
-			}
-			const list = parts.shift();
-			if (!(list in data.lists)) data.lists[list] = {
-				unsorted_items: [],
-			};
-			const inc = parts.shift();
-			if (!inc) {
-				if (data.lists[list].name) {
-					console.warn(`Clash for list id ${list}`);
-					continue;
-				}
-				data.lists[list].name = val;
-				continue;
-			}
-			if (inc === '.increment') {
-				data.lists[list].increment = val;
-				continue;
-			}
-
-			const uuid = uuidv4();
-			let name = val;
-			let url;
-			const match = /^(?<start>.*?)[ \-]*(?<url>https?:\/\/[^ ]+)[ \-]*(?<end>.*?)$/i.exec(val);
-			if (match) {
-				url = match.groups.url;
-				name = match.groups.start;
-				if (match.groups.end) name += " " + match.groups.end;
-				if (!name.trim()) name = url;
-			}
-			data.items[uuid] = {
-				name,
-				increment: inc,
-				list,
-			};
-			if (url) data.items[uuid].url = url;
-			data.lists[list].unsorted_items.push({
-				uuid,
-				inc
-			});
-		}
-		for (const list in data.lists) {
-
-			// Any list without a name at this point is redundant
-			if (!('name' in data.lists[list])) {
-				console.log(`Deleting list ${list}`);
-				delete data.lists[list];
-				continue;
-			}
-
-			data.lists[list].unsorted_items.sort((a,b) => a.inc - b.inc);
-			data.lists[list].items = data.lists[list].unsorted_items.map(item => item.uuid);
-			delete data.lists[list].unsorted_items;
-		}
-		return data;
-	} catch (err) {
-		console.log(`Failed to transform v1 state file`, err);
 	}
 }
 
