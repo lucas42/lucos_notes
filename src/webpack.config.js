4c0330a1d2c51e7fc5055373e03efcc8708e4867
Luke Blaney <git@lukeblaney.co.uk>
2022-05-22 20:27:48 +0100

    Include the hashes of the templates and resources folders at the top of the service-worker file, so that if they change, it'll prompt the browser to re-install the service worker

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -1,12 +1,30 @@
 import { URL } from 'url';
-export default {
-  entry: {
-    client: './client/index.js',
-    serviceworker: './service-worker/index.js',
-  },
-  output: {
-    filename: '[name].js',
-    path: new URL('./resources/', import.meta.url).pathname,
-  },
-  mode: 'production',
+import webpack from 'webpack';
+import TerserPlugin from 'terser-webpack-plugin';
+import { hashElement } from 'folder-hash';
+export default async () => {
+	return {
+		entry: {
+			client: './client/index.js',
+			serviceworker: './service-worker/index.js',
+		},
+		output: {
+			filename: '[name].js',
+			path: new URL('./resources/', import.meta.url).pathname,
+		},
+		plugins: [
+			// Get the hashes of all the resources & templates to embed in a comment in service worker
+			new webpack.BannerPlugin({
+				banner: `Resource Hash: ${(await hashElement("./resources")).hash}\nTemplate Hash: ${(await hashElement("./templates")).hash}`,
+				include: 'serviceworker',
+			}),
+		],
+		optimization: {
+			// Stop the terser plugin messing with the banner plugin
+			minimizer: [new TerserPlugin({
+				extractComments: false,
+			})],
+		},
+		mode: 'production',
+	};
 };
