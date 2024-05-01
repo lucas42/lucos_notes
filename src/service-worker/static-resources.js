63702eecd0d15d3562253ae4b8443bf71902c4ec
Luke Blaney <git@lukeblaney.co.uk>
2024-05-02 00:29:02 +0100

    upgrade navbar to inline logo version, so no cross origin requests need saving in cache now

--- src/service-worker/static-resources.js
+++ src/service-worker/static-resources.js
@@ -5,21 +5,10 @@ const localUrls = [
 	'/client.js',
 	'/ReenieBeanie.ttf',
 ];
-const crossDomainUrls = [
-	'https://l42.eu/logo.png',
-];
 export default async function refresh() {
 	try {
 		const cache = await caches.open(RESOURCE_CACHE);
 		await cache.addAll(localUrls);
-
-		// `addAll` doesn't work for URLs which need a `no-cors` request
-		// Instead need to fetch them individually and call `put`
-		Promise.all(crossDomainUrls.map(async url => {
-			const request = new Request(url, {mode: 'no-cors'})
-			const response = await fetch(request);
-			await cache.put(request, response);
-		}));
 	} catch (error) {
 		console.error("Failed to cache resources:", error.message);
 	}
